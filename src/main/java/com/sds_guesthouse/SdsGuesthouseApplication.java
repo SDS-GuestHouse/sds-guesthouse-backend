7ea429a5e5b3c08623433df9b97f2718193ef10f
scsa2247 <scsa2247@gmail.com>
2026-03-17 17:53:25 +0900

    init commit

---
+++ src/main/java/com/sds_guesthouse/SdsGuesthouseApplication.java
@@ -0,0 +1,13 @@
+package com.sds_guesthouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdsGuesthouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdsGuesthouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sds_guesthouse/SdsGuesthouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sds_guesthouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdsGuesthouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
