3fdcd2a1879675957ac41394bfd0f6c3decfe08e
ohajun12 <142713737+ohajun12@users.noreply.github.com>
2026-03-20 11:22:53 +0900

    fix: API 단수로 변경

--- src/main/java/com/sds_guesthouse/controller/HouseController.java
+++ src/main/java/com/sds_guesthouse/controller/HouseController.java
@@ -13,7 +13,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/v1/houses")
+@RequestMapping("/api/v1/house")
 @RequiredArgsConstructor
 public class HouseController {
 
