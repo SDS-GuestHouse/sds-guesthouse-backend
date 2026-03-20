24d6166902b5cbca2a3af040d3db4008a9b69241
scsa2247 <scsa2247@gmail.com>
2026-03-20 15:41:00 +0900

    fix : wrapper class

--- src/main/java/com/sds_guesthouse/config/SecurityConfig.java
+++ src/main/java/com/sds_guesthouse/config/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.sds_guesthouse.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,20 +24,26 @@ public class SecurityConfig {
         	// 1. CSRF 보안 비활성화 (테스트 편의를 위해, 실무에선 설정 필요)
         	.csrf(csrf -> csrf.disable())
         	
-        	// 2. URL별 권한 제어 (가장 중요한 부분!)
+        	// 2. URL별 권한 제어
             .authorizeHttpRequests(auth -> auth
                 // 호스트 전용 API는 ROLE_HOST 권한이 있어야만 접근 가능
-                .requestMatchers("/api/v1/host/**").hasRole("HOST")
+                .requestMatchers(HttpMethod.POST, "/api/v1/house").hasRole("GUEST")
                 // 로그인, 회원가입 등 공통 API는 누구나 접근 가능
-                .requestMatchers("/api/v1/auth/**", "/signin", "/signup").permitAll()
+                .requestMatchers("/api/v1/host/**", "/api/v1/guest/**", "/api/v1/admin/signin").permitAll()
                 // 그 외 모든 요청은 인증(로그인)이 필요함
                 .anyRequest().authenticated()
             )
             
-            .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll() // 모든 요청을 인증 없이 허용 (로그인 상태 아니어도 인증되도록)
-                // 추후 회원가입, 로그인 등만 permitAll()로 처리해줘야함
-                // 나머지는 **authenticated()**로 설정해서 로그인한 사람만 쓰게 바꿉니다.
+//            .authorizeHttpRequests(auth -> auth
+//                .anyRequest().permitAll() // 모든 요청을 인증 없이 허용 (로그인 상태 아니어도 인증되도록)
+//                // 추후 회원가입, 로그인 등만 permitAll()로 처리해줘야함
+//                // 나머지는 **authenticated()**로 설정해서 로그인한 사람만 쓰게 바꿉니다.
+//            );
+            
+         // 3. 세션 관리 전략
+            .sessionManagement(session -> session
+                // 세션이 없으면 새로 생성하고, 있으면 기존 것 사용 (IF_REQUIRED)
+                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
             );
         return http.build();
 
