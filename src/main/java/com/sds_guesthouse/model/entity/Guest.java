ab71b3a7ab1ef4f203c1dc07e33ca80f5412e546
SeoLanee <seolan25@gmail.com>
2026-03-20 16:33:51 +0900

    Guest: implement Guest entity

---
+++ src/main/java/com/sds_guesthouse/model/entity/Guest.java
@@ -0,0 +1,26 @@
+package com.sds_guesthouse.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString(exclude = "password") // 로그 출력 시 비밀번호 보안 유지
+public class Guest {
+
+    private Long guestId;        // PK (자동 증가분)
+    private String userId;      // 호스트 아이디
+    private String password;    // 암호화된 비밀번호 (Bcrypt)
+    private String name;        // 호스트 이름
+    private String phone;       // 연락처
+    private LocalDateTime createdAt; // 가입 일시
+    private LocalDateTime updatedAt; // 수정 일시
+    
+}
