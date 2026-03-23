3d9260079b45e512f8cb93b78f988e4bcee14121
ohajun12 <142713737+ohajun12@users.noreply.github.com>
2026-03-23 13:27:20 +0900

    feat: 예약 취소 API 개발

--- src/main/java/com/sds_guesthouse/controller/ReservationController.java
+++ src/main/java/com/sds_guesthouse/controller/ReservationController.java
@@ -8,8 +8,9 @@ import com.sds_guesthouse.model.service.ReservationService;
 import com.sds_guesthouse.model.entity.Reservation;
 import com.sds_guesthouse.model.entity.ReservationStatus;
 
-
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/reservation")
@@ -34,5 +35,25 @@ public class ReservationController {
         Reservation reservation = reservationService.getReservationByReservationId(reservationId);
         
         return ResponseEntity.ok(reservation);
+    }
+    
+    @DeleteMapping("/{reservationId}")
+    public ResponseEntity<Map<String, String>> cancelReservation(@PathVariable Long reservationId) {
+        String result = reservationService.cancelReservation(reservationId);
+    	Map<String, String> response = new HashMap<>();
+        switch (result) {
+	        case "NOT_FOUND":
+	            response.put("message", "예약을 찾을 수 없습니다.");
+	            break;
+	        case "ALREADY_CANCELLED":
+	            response.put("message", "예약이 이미 취소된 상태입니다.");
+	            break;
+	        case "SUCCESS":
+	            response.put("message", "예약이 성공적으로 취소되었습니다.");
+	            break;
+	        default:
+	            response.put("message", "알 수 없는 오류 발생");
+       }
+        return ResponseEntity.ok(response);
     }
 }
