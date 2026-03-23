a45433adc871bde9b420c0fdb9b648dbe4569b29
ohajun12 <142713737+ohajun12@users.noreply.github.com>
2026-03-23 17:06:39 +0900

    feat: front origin 허용

--- src/main/java/com/sds_guesthouse/config/WebConfig.java
+++ src/main/java/com/sds_guesthouse/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
-                .allowedOrigins("http://localhost:5173") // 해당 출처 허용
+                .allowedOrigins("http://localhost:5173", "http://192.168.50.131:5173") // 해당 출처 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
                 .allowedHeaders("*")
                 .allowCredentials(true);
