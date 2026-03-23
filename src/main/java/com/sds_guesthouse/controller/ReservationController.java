9fa9dd4015d9a53c7d1dc97148b802bee795d610
ohajun12 <142713737+ohajun12@users.noreply.github.com>
2026-03-23 10:42:16 +0900

    feat: 예약 조회 API 개발

---
+++ src/main/java/com/sds_guesthouse/controller/ReservationController.java
@@ -0,0 +1,31 @@
+package com.sds_guesthouse.controller;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import lombok.RequiredArgsConstructor;
+
+import com.sds_guesthouse.model.service.ReservationService;
+import com.sds_guesthouse.model.entity.Reservation;
+import com.sds_guesthouse.model.entity.ReservationStatus;
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/reservation")
+@RequiredArgsConstructor
+public class ReservationController {
+
+    private final ReservationService reservationService;
+
+    @GetMapping
+    public ResponseEntity<List<Reservation>> getReservations(
+            @RequestParam(required = false) ReservationStatus status,
+            @RequestParam(required = false) String date
+    ) {
+
+        List<Reservation> reservations = reservationService.getReservations(status, date);
+
+        return ResponseEntity.ok(reservations);
+    }
+}
